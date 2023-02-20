d2242a24f3ec866e99c0eab2f9192c0676891542
Fabricio Miranda <fbrnery@gmail.com>
2023-02-20 13:41:08 -0300

    Gerando o Model de Jogos

--- spec/models/system_requirement_spec.rb
+++ spec/models/system_requirement_spec.rb
@@ -8,4 +8,6 @@ it { is_expected.to validate_presence_of(:storage) }
 it { is_expected.to validate_presence_of(:processor) }
 it { is_expected.to validate_presence_of(:memory) }
 it { is_expected.to validate_presence_of(:video_board) }
+it { is_expected.to have_many(:games).dependent(:restrict_with_error) }
+
 end
