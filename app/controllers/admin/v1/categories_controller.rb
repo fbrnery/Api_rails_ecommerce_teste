1d140356d5ffe419a24a6087734782e7de53ab89
Fabricio Miranda <fbrnery@gmail.com>
2023-02-27 18:40:02 -0300

    Cadastro de Categoria e Renderização de Erro(Precisando de correções em spec/requests/admin/v1/categories_request_spec.rb)

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -2,6 +2,22 @@ module Admin::V1
   class CategoriesController < ApplicationController
     def index
       @categories = Category.all
+    end
+
+    def create
+        @category = Category.new
+        @category.attributes = category_params
+
+        save_category!
+    rescue
+        render_error(fields: @category.errors.messages)
+    end
+
+    private
+
+    def category_params
+        return {} unless params.has_key?(:category)
+        params.require(:category).permit(:id, :name)
+    end
   end
-end
 end
